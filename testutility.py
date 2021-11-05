0fd719cfdff79578af64d535b4234ea1c97cb316
Akhil Nair <86709119+oak-hill@users.noreply.github.com>
2021-11-05 18:38:38 +0530

    Add files via upload

---
+++ testutility.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import yaml
+import os
+import subprocess
+import gc
+import re
+import datetime
+import logging
+import gs
+import testutility as util
+
+def read_config_file(filepath):
+    with open(filepath, 'r') as stream:
+        try:
+            return yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            logging.error(exc)
+            
+#Reading the file:
+sample= pd.read_csv("artists.csv", delimiter=',')   
